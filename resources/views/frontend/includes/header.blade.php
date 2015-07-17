32bd3a3d7aa6c094f271f62eb45638932bba0886
Eugenio Gomez <eugenio.gomez@heliocentrix.co.uk>
2015-07-17 16:43:08 +0100

    Logout btn

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -1,5 +1,10 @@
 <div class="header text-center-sm">
 	<div class="headerContainer">
+		@if(Auth::user())
+			<div class="col-xs-4 col-xs-offset-8">
+				<a class="logout-btn" href="/auth/logout"></a>
+			</div>
+		@endif
 		<img class="col-xs-offset-2 headerLogo" src="/images/dbslogo.svg" width="173" height="104" alt="DBS Contracts" data-mu-svgfallback="images/dbslogo_poster_.png">
 		<div class="hidden-xs headerPhone col-sm-offset-8">
 			01622 715 919
