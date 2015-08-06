9b8c4de17e7585dd299c3b179721d6011ed2cb9c
Eugenio Gomez <eugenio.gomez@heliocentrix.co.uk>
2015-08-06 12:21:19 +0100

    Approve logos and error msg fixing

--- resources/views/backend/includes/errors.blade.php
+++ resources/views/backend/includes/errors.blade.php
@@ -1,32 +1,20 @@
 @if (count($errors) > 0)
-    <div class="col-xs-12">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="alert alert-danger">
-                <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
 @endif
 @if (Session::get('success'))
-    <div class="col-xs-12">
-        <div class="col-md-8 col-md-offset-2">
-        	<div class="alert alert-success">
-        		{{ Session::get('success') }}
-        	</div>
-        </div>
-    </div>
+	<div class="alert alert-success">
+		{{ Session::get('success') }}
+	</div>
 @endif
 @if (isset($msg) && $msg)
-    <div class="col-xs-12">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="alert alert-success">
-                {{$msg}}
-            </div>
-        </div>
+    <div class="alert alert-success">
+        {{$msg}}
     </div>
 @endif
