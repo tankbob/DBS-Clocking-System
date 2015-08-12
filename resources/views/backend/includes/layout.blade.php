0f9e5de2e587a709bb536d131af3718fe3409016
Adam Tester <adam@genyx.co.uk>
2015-08-12 17:33:01 +0100

    Move JS

--- resources/views/backend/includes/layout.blade.php
+++ resources/views/backend/includes/layout.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/css/bootstrap-theme.min.css">
     <link rel="stylesheet" type="text/css" href="/css/admin.css">
+    <script type="text/javascript" src="/js/jquery-1.11.3.min.js"></script>
 </head>
 <body class="@yield('body-class')">
     <div class="header text-center-sm">
@@ -57,7 +58,7 @@
         </div>
     </footer>
 
-    <script type="text/javascript" src="/js/jquery-1.11.3.min.js"></script>
+    
     <script type="text/javascript" src="/js/bootstrap.min.js"></script>
     <script src="/js/jquery.dataTables.min.js"></script>
     @yield('scripts')
