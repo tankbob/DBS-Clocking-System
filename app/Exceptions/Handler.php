cfcd1d7329245e2e47f696b968dd40683aa07515
Eugenio Gomez <eugenio.gomez@heliocentrix.co.uk>
2015-12-17 11:44:32 +0000

    that's actually working

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -16,7 +16,7 @@ class Handler extends ExceptionHandler
      */
     protected $dontReport = [
         HttpException::class,
-        Illuminate\Session\TokenMismatchException::class,
+        TokenMismatchException::class,
     ];
 
     /**
